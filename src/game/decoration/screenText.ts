82d9c95c961a54ba2663097cd45237ec57d104fc
Enkhdavaa Batlkhagva <enkhee3469@gmail.com>
2025-04-05 10:17:30 +0200

    Moved to public folder

--- src/game/decoration/screenText.ts
+++ src/game/decoration/screenText.ts
@@ -5,31 +5,28 @@ import scene from "../scene.ts";
 
 const fontLoader = new FontLoader();
 const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("assets/textures/matcap/1.png");
+const matcapTexture = textureLoader.load("/textures/matcap/1.png");
 
 export const RenderScreenText = () => {
-  fontLoader.load(
-    "assets/fonts/helvetiker_regular.typeface.json",
-    (font: any) => {
-      const textGeometry = new TextGeometry("The Cubs Run Club", {
-        font: font,
-        size: 0.5,
-        depth: 0.2,
-        curveSegments: 5,
-        bevelEnabled: true,
-        bevelThickness: 0.1,
-        bevelSize: 0.02,
-        bevelOffset: 0,
-        bevelSegments: 4,
-      });
+  fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font: any) => {
+    const textGeometry = new TextGeometry("The Cubs Run Club", {
+      font: font,
+      size: 0.5,
+      depth: 0.2,
+      curveSegments: 5,
+      bevelEnabled: true,
+      bevelThickness: 0.1,
+      bevelSize: 0.02,
+      bevelOffset: 0,
+      bevelSegments: 4,
+    });
 
-      textGeometry.center();
+    textGeometry.center();
 
-      const textMaterial = new THREE.MeshMatcapMaterial({
-        matcap: matcapTexture,
-      });
-      const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-      scene.add(textMesh);
-    }
-  );
+    const textMaterial = new THREE.MeshMatcapMaterial({
+      matcap: matcapTexture,
+    });
+    const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+    scene.add(textMesh);
+  });
 };
